d8ad3d59a35b0f120a8a8ab3884380f3b9dc2c53
Ana Novak <anastasy.d@gmail.com>
2021-01-23 20:45:05 +0100

    Add prettier config

--- emails-input.ts
+++ emails-input.ts
@@ -1,11 +1,11 @@
 function EmailsInput(rootNode: Element | null) {
     if (!rootNode) {
-        throw new Error('Missing root node')
+        throw new Error('Missing root node');
     }
-    console.log('init emails input in', rootNode)
+    console.log('init emails input in', rootNode);
     return {
         addEmail: (email: string) => {
-            console.log('!!!', email)
-        }
-    }
+            console.log('!!!', email);
+        },
+    };
 }
