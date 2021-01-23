48b2a7629b74a95a41972bbec2101e8588939fed
Ana Novak <anastasy.d@gmail.com>
2021-01-23 22:05:38 +0100

    Update layout

--- emails-input.ts
+++ emails-input.ts
@@ -2,7 +2,25 @@ function EmailsInput(rootNode: Element | null) {
     if (!rootNode) {
         throw new Error('Missing root node');
     }
-    console.log('init emails input in', rootNode);
+
+    rootNode.setAttribute(
+        'style',
+        `
+        background: #fff;
+        height: 90px;
+        padding: 3px;
+        cursor: text;
+    `,
+    );
+
+    rootNode.addEventListener('click', () => {
+        input.focus();
+    });
+
+    const input = document.createElement('input');
+    input.setAttribute('placeholder', 'add more people...');
+    rootNode.appendChild(input);
+
     return {
         addEmail: (email: string) => {
             console.log('!!!', email);
