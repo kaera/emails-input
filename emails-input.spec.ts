40d6faa47a9f0ccc151aaa932fddc8465c2bb036
Ana Novak <anastasy.d@gmail.com>
2021-01-24 02:12:38 +0100

    Implement email adding by typing comma and removing

--- emails-input.spec.ts
+++ emails-input.spec.ts
@@ -8,10 +8,13 @@ describe(__filename, () => {
         rootNode = document.createElement('div');
         document.body.appendChild(rootNode);
         component = EmailsInput(rootNode);
-        component.addEmail('foo@example.com');
     });
 
     describe('API', () => {
+        beforeEach(() => {
+            component.addEmail('foo@example.com');
+        });
+
         it('should return email count', () => {
             expect(component.getEmailCount()).toBe(1);
         });
@@ -28,9 +31,35 @@ describe(__filename, () => {
     });
 
     describe('DOM', () => {
-        it('should render email tags', () => {
+        it('should render valid email tag', () => {
+            component.addEmail('foo@example.com');
             const emailTag = rootNode.querySelector('.email-tag');
             expect(emailTag?.textContent).toBe('foo@example.com');
+            expect(emailTag?.classList.contains('valid')).toBeTruthy();
+        });
+
+        it('should render invalid email tags', () => {
+            component.addEmail('foo');
+            const emailTag = rootNode.querySelector('.email-tag');
+            expect(emailTag?.textContent).toBe('foo');
+            expect(emailTag?.classList.contains('invalid')).toBeTruthy();
+        });
+
+        it('should remove email tags', () => {
+            component.addEmail('foo@example.com');
+            const removeButton = rootNode.querySelector(
+                '.remove-button',
+            ) as HTMLButtonElement;
+            removeButton.click();
+            expect(component.getEmailCount()).toBe(0);
+            expect(rootNode.querySelector('.email-tag')).toBeNull();
+        });
+
+        it('should add email after typing comma', () => {
+            const input = rootNode.querySelector('.input') as HTMLInputElement;
+            input.value = 'foo@example.com,';
+            input.dispatchEvent(new InputEvent('input', { data: ',' }));
+            expect(component.getEmailCount()).toBe(1);
         });
     });
 });
--- emails-input.ts
+++ emails-input.ts
@@ -8,54 +8,102 @@ export function EmailsInput(rootNode: Element | null) {
         throw new Error('Missing root node');
     }
 
-    rootNode.setAttribute(
-        'style',
-        `
-        background: #fff;
-        height: 90px;
-        padding: 3px;
-        cursor: text;
-    `,
-    );
-
-    rootNode.addEventListener('click', () => {
-        input.focus();
+    rootNode.classList.add('emails-input');
+    rootNode.addEventListener('click', (e) => {
+        const target = e.target as HTMLElement;
+        if (target.className === 'remove-button') {
+            const emailTag = target.parentNode as Node;
+            const index = Array.from(rootNode.childNodes).indexOf(
+                emailTag as ChildNode,
+            );
+            emails.splice(index, 1);
+            rootNode.removeChild(emailTag);
+        } else {
+            input.focus();
+        }
     });
 
     const input = document.createElement('input');
     input.setAttribute('placeholder', 'add more people...');
-    input.setAttribute('style', 'border: 0; outline: 0');
+    input.className = 'input';
+    input.addEventListener('input', (e) => {
+        if ((e as InputEvent).data === ',') {
+            const value = input.value.slice(0, -1);
+            if (value) {
+                addEmail(value);
+            }
+            input.value = '';
+        }
+    });
     rootNode.appendChild(input);
+
+    const style = document.createElement('style');
+    style.textContent = `
+        .emails-input {
+            border: 1px solid #c3c2cf;
+            border-radius: 4px;
+            overflow: auto;
+            background: #fff;
+            height: 88px;
+            padding: 4px 7px;
+            cursor: text;
+            font-size: 14px;
+            line-height: 24px;
+            color: #050038;
+        }
+        .email-tag {
+            margin: 4px 8px 0 0;
+            display: inline-block;
+        }
+
+        .valid {
+            background: rgba(102, 153, 255, 0.2);
+            border-radius: 100px;
+            padding: 0 10px;
+        }
+
+        .invalid {
+            border-bottom: 1px dashed #d92929;
+        }
+
+        .remove-button {
+            display: inline-block;
+            width: 8px;
+            height: 8px;
+            margin-left: 8px;
+            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 0.8L7.2 0L4 3.2L0.8 0L0 0.8L3.2 4L0 7.2L0.8 8L4 4.8L7.2 8L8 7.2L4.8 4L8 0.8Z' fill='currentColor'/%3E%3C/svg%3E");
+            cursor: pointer;
+        }
+
+        .input {
+            border: 0;
+            outline: 0;
+            display: inline-block;
+            margin-top: 4px;
+        }
+    `;
+    rootNode.appendChild(style);
 
     const emails: Email[] = [];
 
-    function isValidEmail(email: string) {
-        return /^[^\s,@]+@[^\s,@]+$/.test(email);
-    }
-    return {
-        addEmail: (email: string): void => {
-            emails.push({
-                value: email,
-                isValid: isValidEmail(email),
-            });
-            const emailTag = document.createElement('span');
-            emailTag.textContent = email;
-            emailTag.className = 'email-tag';
-            emailTag.setAttribute(
-                'style',
-                `
-                margin: 4px 8px 4px 0;
-                font-size: 14px;
-                line-height: 24px;
-                display: inline-block;
+    const addEmail = (email: string): void => {
+        const isValid = /^[^\s,@]+@[^\s,@]+$/.test(email);
+        emails.push({
+            value: email,
+            isValid,
+        });
+        const emailTag = document.createElement('span');
+        emailTag.textContent = email;
+        emailTag.classList.add('email-tag');
+        emailTag.classList.add(isValid ? 'valid' : 'invalid');
+        const removeBtn = document.createElement('span');
+        removeBtn.className = 'remove-button';
+        emailTag.appendChild(removeBtn);
+        rootNode.insertBefore(emailTag, input);
+    };
 
-                background: rgba(102, 153, 255, 0.2);
-                border-radius: 100px;
-                padding: 0 24px 0 10px;
-            `,
-            );
-            rootNode.insertBefore(emailTag, input);
-        },
+    return {
+        addEmail,
         getEmailCount: (): number =>
             new Set(
                 emails
